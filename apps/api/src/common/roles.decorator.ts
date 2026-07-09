725ef075120d690d800bf83d389fd1bd8e9ff039
Abhay Rana <abhayrana8272@gmail.com>
2026-07-10 02:17:16 +0530

    Clean up backend files for frontend-only repository

--- apps/api/src/common/roles.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { Role } from '@vexor/types';
-
-export const ROLES_KEY = 'roles';
-export const Roles = (...roles: (Role | string)[]) => SetMetadata(ROLES_KEY, roles);
